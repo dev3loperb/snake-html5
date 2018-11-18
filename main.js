bac04c7219e39318fe59bf3f0778e69bd9d61c65
Igor Pergenitsa <ipergenitsa@gmail.com>
2018-11-18 23:24:55 +0600

    Adjust size settings of canvas Add a food item on game field

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 let canvas = document.getElementById("game-canvas");
 let ctx = canvas.getContext("2d");
-
-clearCanvas(canvas);
-
-ctx.fillStyle = "red";
-ctx.globalAlpha = 0.3;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+canvas.width = 0.5 * window.innerWidth;
+canvas.height = 0.5 * window.innerHeight;
 
 function clearCanvas(canvas) {
     let ctx = canvas.getContext("2d");
@@ -19,37 +15,54 @@ class SnakeItem {
         this.x = x;
         this.y = y;
         this.image = image;
+        this.direction = Direction.right;
     }
 }
 
-var headImage = new Image();
+const headImage = new Image();
 headImage.src = "img/head.png";
 
-var bodyImage = new Image();
+const bodyImage = new Image();
 bodyImage.src = "img/body.png";
 
-var tailImage = new Image();
+const tailImage = new Image();
 tailImage.src = "img/tail.png";
 
-var step = function () {
-
-};
-
-var render = function () {
-    console.log("rendering");
-    for (var i in snake) {
-        ctx.drawImage(snake[i].image, snake[i].x * 10, snake[i].y * 10);
+const step = function () {
+    for (let index in snake) {
+        switch (snake[index].direction) {
+            case Direction.right:
+                console.log("RIGHT");
+                snake[index].x++;
+                break;
+            case Direction.left:
+                snake[index].x--;
+                break;
+        }
     }
 };
 
-var SnakeType = {"head": 1, "body": 2, "tail": 3};
-var Direction = {"up": 1, "left": 2, "down": 3, "right": 4};
-var snakeHead = new SnakeItem(SnakeType.head, 10, 10, headImage);
-snakeHead.direction = Direction.right;
-var snake = [snakeHead, new SnakeItem(SnakeType.body, 9, 10, bodyImage), new SnakeItem(SnakeType.tail, 8, 10, tailImage)];
+const render = function () {
+    console.log("rendering");
+    clearCanvas(canvas);
+    for (let index in snake) {
+        ctx.drawImage(snake[index].image, snake[index].x * 20, snake[index].y * 20);
+    }
+    ctx.drawImage(food.image, food.x * 20, food.y * 20);
+};
+
+const SnakeType = {"head": 1, "body": 2, "tail": 3};
+const Direction = {"up": 1, "left": 2, "down": 3, "right": 4};
+const snakeHead = new SnakeItem(SnakeType.head, 5, 5, headImage);
+const snake = [snakeHead, new SnakeItem(SnakeType.body, 4, 5, bodyImage), new SnakeItem(SnakeType.tail, 3, 5, tailImage)];
+const food = {};
+food.image = new Image();
+food.image.src = "img/food.png";
+food.x = Math.random() * canvas.width / 20;
+food.y = Math.random() * canvas.height / 20;
 
 render();
 setInterval(function () {
     step();
     render();
-}, 1000);
+}, 500);
