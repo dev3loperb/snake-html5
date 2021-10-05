ad1cd08cf0e4efea9e5d8f4836e6bac78f93fffe
Igor P <ipergenitsa@gmail.com>
2021-10-05 23:39:25 +0300

    Draw snake correctly

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ let ctx = canvas.getContext("2d");
 
 clearCanvas(canvas);
 
-ctx.fillStyle = "red";
-ctx.globalAlpha = 0.3;
+ctx.fillStyle = "green";
+let oldGlobalAlpha = ctx.globalAlpha
+ctx.globalAlpha = 0.1;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.globalAlpha = oldGlobalAlpha;
+
 
 function clearCanvas(canvas) {
     let ctx = canvas.getContext("2d");
@@ -37,15 +40,22 @@ const step = function () {
 const render = function () {
     console.log("rendering");
     for (const i in snake) {
-        ctx.drawImage(snake[i].image, snake[i].x * 10, snake[i].y * 10);
+        let width = snake[i].image.width
+        let height = snake[i].image.height
+        ctx.drawImage(snake[i].image, snake[i].x * width, snake[i].y * height);
     }
 };
 
 const SnakeType = {"head": 1, "body": 2, "tail": 3};
 const Direction = {"up": 1, "left": 2, "down": 3, "right": 4};
-const snakeHead = new SnakeItem(SnakeType.head, 10, 10, headImage);
+const snakeHead = new SnakeItem(SnakeType.head, 7, 2, headImage);
 snakeHead.direction = Direction.right;
-const snake = [snakeHead, new SnakeItem(SnakeType.body, 9, 10, bodyImage), new SnakeItem(SnakeType.tail, 8, 10, tailImage)];
+const snake = [snakeHead,
+    new SnakeItem(SnakeType.body, 6, 2, bodyImage),
+    new SnakeItem(SnakeType.body, 5, 2, bodyImage),
+    new SnakeItem(SnakeType.body, 4, 2, bodyImage),
+    new SnakeItem(SnakeType.tail, 3, 2, tailImage)
+];
 
 render();
 setInterval(function () {
